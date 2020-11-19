44f7dec1dbdc1175a8f609542d001f6606b003e6
StanislasE <stanislas.etienne@gmail.com>
2020-11-19 10:31:12 +0100

    fait

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    $departments = array (
+        02 => 'L\'Aisne'  ,
+        59 => 'Nord' ,
+        60 => 'Oise' ,
+        62 => 'Pas de Calais' ,
+        80 => 'Sommes' 
+    );
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercice 4</title>
+</head>
+<body>
+    
+</body>
+</html>
